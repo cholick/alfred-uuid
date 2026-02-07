468a65425138714e7a172871961ae20950562660
Matt Cholick <cholick@gmail.com>
2026-02-07 10:20:59 -0800

    Fix path for linting info.plist

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,6 +4,8 @@ set -e
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${script_dir}/.."
 
+plutil -lint workflow/info.plist
+
 mkdir -p /tmp/alfred-uuid
 
 cp workflow/icon.png /tmp/alfred-uuid
@@ -11,8 +13,6 @@ cp workflow/info.plist /tmp/alfred-uuid
 
 version=$(cat workflow/version)
 sed -i '' "s/VERSION_PLACEHOLDER/$version/g" /tmp/alfred-uuid/info.plist
-
-plutil -lint /tmp/alfred-encode-decode/info.plist
 
 cd /tmp/alfred-uuid
 
